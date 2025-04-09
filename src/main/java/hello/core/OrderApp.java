c42efbc66a2a46dced54151e0f9a2d2eb25c3536
Yuyeseul <ryuys0308@naver.com>
2025-04-09 15:52:40 +0900

    스프링으로 전환

--- src/main/java/hello/core/OrderApp.java
+++ src/main/java/hello/core/OrderApp.java
@@ -5,13 +5,20 @@ import hello.core.member.Member;
 import hello.core.member.MemberService;
 import hello.core.order.Order;
 import hello.core.order.OrderService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class OrderApp {
 
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService(); // MemberServiceImpl
-        OrderService orderService = appConfig.orderService(); // OrderServiceImpl
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService(); // MemberServiceImpl
+//        OrderService orderService = appConfig.orderService(); // OrderServiceImpl
+
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+        OrderService orderService = applicationContext.getBean("orderService", OrderService.class);
 
         Long memberId = 1L;
         Member member = new Member(memberId, "member1", Grade.VIP);
