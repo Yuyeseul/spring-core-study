12079221731966b9f84ff48e87bfd5e3c2092a49
Yuyeseul <ryuys0308@naver.com>
2025-04-10 21:09:27 +0900

    필터 사용 연습

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -6,8 +6,9 @@ import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @ComponentScan(
+        basePackages = "hello.core", // 탐색 시작 위치 지정
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION,
-                classes = Configuration.class)
+                classes = Configuration.class) // AppConfig 충돌을 피하기 위해 예외 처리
 )
 public class AutoAppConfig {
 }
