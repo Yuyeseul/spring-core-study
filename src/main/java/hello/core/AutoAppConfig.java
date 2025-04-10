16c2304e3462212abb7a4079e2f1248c56fd209f
Yuyeseul <ryuys0308@naver.com>
2025-04-10 21:54:32 +0900

    빈 중복 등록과 충돌

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -1,5 +1,8 @@
 package hello.core;
 
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
@@ -11,4 +14,10 @@ import org.springframework.context.annotation.FilterType;
                 classes = Configuration.class) // AppConfig 충돌을 피하기 위해 예외 처리
 )
 public class AutoAppConfig {
+
+    // 수동 빈이 자동 빈을 오버라이딩 함
+    @Bean(name = "memoryMemberRepository")
+    MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
 }
